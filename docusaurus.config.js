c10ed127f582163ffbee13f973e60cd81f1c4802
Simon Ho <simon.ho@commit.dev>
2021-08-25 11:42:17 -0700

    add contribute section

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,16 +21,20 @@ module.exports = {
       },
       items: [
         {
-          type: 'doc',
           docId: 'intro',
-          position: 'left',
           label: 'Tutorial',
+          position: 'left',
+          type: 'doc',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+          label: 'Blog',
+          position: 'left',
+          to: '/blog',
+        },
+        {
+          label: 'Contribute',
+          position: 'left',
+          to: '/contribute',
         },
       ],
     },
@@ -92,13 +96,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-          'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/facebook/docusaurus/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-          'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
---
+++ src/pages/contribute/index.md
@@ -0,0 +1,14 @@
+# Contribute
+
+We appreciate all the help we can get on this project!
+
+## Everyone
+
+Even without coding experience, you can help us out by:
+
+- [Reporting bugs](https://github.com/commitdev/companion/issues/new?template=bug_report.md&title=Bug%3A+)
+- [Suggesting features and ideas](https://github.com/commitdev/companion/issues/new?template=feature_request.md&title=Feature%3A+)
+
+## Developers
+
+Please submit pull requests or fix issues in our [GitHub repo](https://github.com/commitdev/companion).
